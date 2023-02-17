ff737dadc7cbfeb883b9e62c1262ad8fd2385132
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-17 21:29:21 +0530

    Update lambda_function_1.py

--- lambda/cod1/lambda_function_1.py
+++ lambda/cod1/lambda_function_1.py
@@ -1,3 +1,4 @@
 def lambda_handler (event, context):
    print ("Welcome to Second Lambda Fumction Pipeline")
    print ("Welome to Persistent Systems")
+   print ("This is Lambda Function Testing")
