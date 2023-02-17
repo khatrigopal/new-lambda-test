f79e94854c9895350dfe8101b152baba29f2463b
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-17 21:06:44 +0530

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -3,6 +3,6 @@ module "lambda_functions_1" {
   function_name = "lambda_function_1"
   role_arn = "arn:aws:iam::558940753150:role/lambda-full-acces"
   handler = "lambda_function_1.lambda_handler"
-  filename = "${path.module}/cod1/"
+  filename = "${path.module}/cod1/lambda_function_1.zip"
    #filename = "${path.module}/cod1/lambda_function_1.zip"
 }
