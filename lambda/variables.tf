7f1173c6501737794a8f566e5773ed181135fa10
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-17 20:24:24 +0530

    Update variables.tf

--- lambda/variables.tf
+++ lambda/variables.tf
@@ -1,9 +1,20 @@
-variable "lambda_functions" {
-  type = map(object({
-    source_path = string
-    handler = string
-    runtime = string
-    memory_size = number
-    timeout = number
-  }))
+variable "filename" {
+  description = "The filename of the Python file for the AWS Lambda function."
+  default = ""
+}
+
+
+variable "role_arn" {
+  description = "The ARN of the IAM role for the AWS Lambda function."
+}
+
+
+variable "function_name" {
+  description = "The name of the AWS Lambda function."
+  default = ""
+}
+
+
+variable "handler" {
+  description = "The name of the Python function that AWS Lambda calls to start execution."
 }
