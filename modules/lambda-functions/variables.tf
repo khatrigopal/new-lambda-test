c75265c4f9e1e2edf04a100f2a124668a8677cfb
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-17 19:57:28 +0530

    Create variables.tf

---
+++ modules/lambda-functions/variables.tf
@@ -0,0 +1,20 @@
+variable "filename" {
+  description = "The filename of the Python file for the AWS Lambda function."
+  default = ""
+}
+
+
+variable "role_arn" {
+  description = "The ARN of the IAM role for the AWS Lambda function."
+}
+
+
+variable "function_name" {
+  description = "The name of the AWS Lambda function."
+  default = ""
+}
+
+
+variable "handler" {
+  description = "The name of the Python function that AWS Lambda calls to start execution."
+}
