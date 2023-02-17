2f435fbd10fb3ca5225de41835abfda29580c331
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-18 00:06:04 +0530

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -4,7 +4,7 @@ module "lambda_functions_1" {
   role_arn = "arn:aws:iam::558940753150:role/lambda-full-acces"
   handler = "lambda_function_1.lambda_handler"
   #filename = "${path.module}/cod1/lambda_function_1.py"
-   filename = "cod1/lambda_function_1.zip"
+   source_path = "cod1/lambda_function_1.zip"
 }
   
   
@@ -13,6 +13,6 @@ module "lambda_function_2" {
   function_name = "lambda_fn_2"
   role_arn = "arn:aws:iam::558940753150:role/lambda-full-acces"
   handler = "lambda_function_2.lambda_handler"
-  filename = "code2/lambda_function_2.zip"
+  source_path = "code2/lambda_function_2.zip"
   
 }
