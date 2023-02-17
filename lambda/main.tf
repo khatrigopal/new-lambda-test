4b4c53033c0322c3b5d7513ea9604a0df2fd3e38
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-17 20:11:56 +0530

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,5 +1,5 @@
 module "lambda_functions_1" {
-  source = "../modules/lambda_functions"
+  source = "..modules/lambda-functions"
   function_name = "lambda_function_1"
   role_arn = "arn:aws:iam::123456789012:role/lambda-role"
   handler = "lambda_function_1.handler"
