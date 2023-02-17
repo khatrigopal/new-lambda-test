30cdce78ee3097171a9a83428b52087f883df5d8
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-17 21:54:19 +0530

    Update lambda_function_2.py

--- lambda/code2/lambda_function_2.py
+++ lambda/code2/lambda_function_2.py
@@ -2,3 +2,4 @@ def lambda_handler (event, context):
    print ("Welcome to new Lambda Fumction Pipeline")
    print ("This is for Testing")
    print ("Lambda Pipeline Testing Phase")
+    print ("This is line no. 5")
